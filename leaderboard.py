f4936e6ef9501ba82e314c5e6170f3b6cdb45aa1
Rae <tmmx2b@umsystem.edu>
2022-04-19 02:30:43 -0500

    tried to improve leaderboard visuals

--- leaderboard.py
+++ leaderboard.py
@@ -12,7 +12,8 @@
 # Pygame Documentation: https://www.pygame.org/docs/ 
 # Geeks-for-Geeks Tutorials: https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
 # https://www.geeksforgeeks.org/python-add-new-keys-to-a-dictionary/ 
-# Dictionary Sorting with Lambda: https://www.youtube.com/watch?v=6uih8-Cc7Cg&ab_channel=JieJenn  
+# Dictionary Sorting with Lambda: https://www.youtube.com/watch?v=6uih8-Cc7Cg&ab_channel=JieJenn
+# RGB Calculator: https://www.w3schools.com/colors/colors_rgb.asp   
 # =========================================================================================================
 import pygame
 import sys
@@ -29,10 +30,32 @@ offset = 0.0
 
 def format_leaderboard(root, screen):
     # Setting up leaderboard appearance
-    BACKGROUND_COLOR = Color(80, 80, 245)
-    EASY_BACKGROUND_COLOR = Color(82, 216, 50)
-    HARD_BACKGROUND_COLOR = Color(100, 70, 50)
+
+    # Color List
+    black = Color(0, 0, 0)
+    blue = Color(58, 121, 227)
+    light_blue = Color(30, 30, 230)
+    lighter_blue = Color(80, 80, 245)
+    brown = Color(100, 70, 50)
+    green = Color(82, 216, 50)
+    purple = Color(112, 65, 192)
+    light_purple = Color(186, 156, 199)
+    pink = Color(203, 165, 188)
+    magenta = Color(172, 89, 188)
+    teal = Color(41, 106, 131)
+    light_teal = Color(98, 184, 208)
+
+    BACKGROUND_COLOR = Color(34, 26, 92)
+    EASY_BACKGROUND_COLOR = black
+    HARD_BACKGROUND_COLOR = black
     TEXT_COLOR = Color(255, 255, 255)
+    HEADER_COLOR_EASY = teal
+    HEADER_COLOR_HARD = teal
+    SUBHEADER_COLOR_EASY = teal
+    SUBHEADER_COLOR_HARD = teal
+    MESSAGE_BACKGROUND_COLOR = teal
+    ENTRY_BACKGROUND_1 = Color(54, 154, 181)
+    ENTRY_BACKGROUND_2 = light_teal
 
     # Font
     header_font = pygame.font.SysFont(None, 75)
@@ -58,14 +81,6 @@ def format_leaderboard(root, screen):
     header_text_easy = "Best Scores: Easy Difficulty"
     header_text_hard = "Best Scores: Hard Difficulty"
     subheader_text = "Rank                    Name                    Score"
-    
-    # Color List
-    black = Color(0, 0, 0)
-    blue = Color(0, 0, 200)
-    light_blue = Color(30, 30, 230)
-    lighter_blue = Color(80, 80, 245)
-    brown = Color(100, 70, 50)
-    green = Color(82, 216, 50)
 
     # Other variables
     MAX_ENTRIES = 15
@@ -74,29 +89,29 @@ def format_leaderboard(root, screen):
     screen.fill(BACKGROUND_COLOR)
     pygame.draw.rect(screen, EASY_BACKGROUND_COLOR, easy_rect)
     pygame.draw.rect(screen, HARD_BACKGROUND_COLOR, hard_rect)
-    pygame.draw.rect(screen, blue, header_zone_easy)
-    pygame.draw.rect(screen, blue, header_zone_hard)
-    pygame.draw.rect(screen, black, subheader_zone_easy)
-    pygame.draw.rect(screen, black, subheader_zone_hard)
+    pygame.draw.rect(screen, HEADER_COLOR_EASY, header_zone_easy)
+    pygame.draw.rect(screen, HEADER_COLOR_HARD, header_zone_hard)
+    pygame.draw.rect(screen, SUBHEADER_COLOR_EASY, subheader_zone_easy)
+    pygame.draw.rect(screen, SUBHEADER_COLOR_HARD, subheader_zone_hard)
 
     # Fill in the header information
-    header_object_easy = header_font.render(header_text_easy, True, TEXT_COLOR, blue)
+    header_object_easy = header_font.render(header_text_easy, True, TEXT_COLOR, HEADER_COLOR_EASY)
     header_rect_easy = header_object_easy.get_rect()
     header_rect_easy.center = ((screen_width/4), 150)
     screen.blit(header_object_easy, header_rect_easy)
 
-    header_object_hard = header_font.render(header_text_hard, True, TEXT_COLOR, blue)
+    header_object_hard = header_font.render(header_text_hard, True, TEXT_COLOR, HEADER_COLOR_HARD)
     header_rect_hard = header_object_hard.get_rect()
     header_rect_hard.center = ((screen_width/4) + x_offset, 150)
     screen.blit(header_object_hard, header_rect_hard)
 
     # Fill in the subheader information
-    subheader_object_easy = subheader_font.render(subheader_text, True, TEXT_COLOR, light_blue)
+    subheader_object_easy = subheader_font.render(subheader_text, True, TEXT_COLOR, SUBHEADER_COLOR_EASY)
     subheader_rect_easy = subheader_object_easy.get_rect()
     subheader_rect_easy.center = ((screen_width/4), 245)
     screen.blit(subheader_object_easy, subheader_rect_easy)
 
-    subheader_object_hard = subheader_font.render(subheader_text, True, TEXT_COLOR, light_blue)
+    subheader_object_hard = subheader_font.render(subheader_text, True, TEXT_COLOR, SUBHEADER_COLOR_HARD)
     subheader_rect_hard = subheader_object_hard.get_rect()
     subheader_rect_hard.center = ((screen_width/4) + x_offset, 245)
     screen.blit(subheader_object_hard, subheader_rect_hard)
@@ -112,10 +127,10 @@ def format_leaderboard(root, screen):
         
         # Draw surface
         easy_message_zone = Rect(100, starting_offset, entry_x_offset_easy, 700)
-        pygame.draw.rect(screen, black, easy_message_zone)
+        pygame.draw.rect(screen, MESSAGE_BACKGROUND_COLOR, easy_message_zone)
 
         # Display message
-        message_object_easy = subheader_font.render(message_text_easy, True, TEXT_COLOR, green)
+        message_object_easy = subheader_font.render(message_text_easy, True, TEXT_COLOR, MESSAGE_BACKGROUND_COLOR)
         message_rect_easy = message_object_easy.get_rect()
         message_rect_easy.center = ((screen_width/4), 600)
         screen.blit(message_object_easy, message_rect_easy)
@@ -140,9 +155,9 @@ def format_leaderboard(root, screen):
         # Format and display the high scores
         for x in range (0, MAX_ENTRIES): # Alternating the zone colors
             if x % 2 == 0:
-                zone_color_easy = black
+                zone_color_easy = ENTRY_BACKGROUND_1
             else:
-                zone_color_easy = brown
+                zone_color_easy = ENTRY_BACKGROUND_2
             
             # Draw the entry zones
             entry_zone_easy = Rect(100, starting_offset + (x * y_offset), entry_x_offset_easy, 45)
@@ -164,7 +179,7 @@ def format_leaderboard(root, screen):
                 name_data_easy = ""
 
             # Render name data
-            name_object_easy = entry_font.render(name_data_easy, True, TEXT_COLOR, blue)
+            name_object_easy = entry_font.render(name_data_easy, True, TEXT_COLOR, zone_color_easy)
             name_rect_easy = name_object_easy.get_rect()
             name_rect_easy.center = ((screen_width/4), 310 + (x * y_offset))
             screen.blit(name_object_easy, name_rect_easy)
@@ -179,7 +194,7 @@ def format_leaderboard(root, screen):
                 score_data_easy = ""
 
             # Render score data
-            score_object_easy = entry_font.render(score_data_easy, True, TEXT_COLOR, blue)
+            score_object_easy = entry_font.render(score_data_easy, True, TEXT_COLOR, zone_color_easy)
             score_rect_easy = score_object_easy.get_rect()
             score_rect_easy.center = ((screen_width/4) + 300, 310 + (x * y_offset))
             screen.blit(score_object_easy, score_rect_easy)
@@ -190,10 +205,10 @@ def format_leaderboard(root, screen):
         
         # Draw surface
         hard_message_zone = Rect(100 + x_offset, starting_offset, entry_x_offset_hard, 700)
-        pygame.draw.rect(screen, black, hard_message_zone)
+        pygame.draw.rect(screen, MESSAGE_BACKGROUND_COLOR, hard_message_zone)
 
         # Display message
-        message_object_hard = subheader_font.render(message_text_hard, True, TEXT_COLOR, green)
+        message_object_hard = subheader_font.render(message_text_hard, True, TEXT_COLOR, MESSAGE_BACKGROUND_COLOR)
         message_rect_hard = message_object_hard.get_rect()
         message_rect_hard.center = ((screen_width/4) + x_offset, 600)
         screen.blit(message_object_hard, message_rect_hard)
@@ -218,9 +233,9 @@ def format_leaderboard(root, screen):
         # Format and display the high scores
         for x in range (0, MAX_ENTRIES): # Alternating the zone colors
             if x % 2 == 0:
-                zone_color_hard = black
+                zone_color_hard = ENTRY_BACKGROUND_1
             else:
-                zone_color_hard = light_blue
+                zone_color_hard = ENTRY_BACKGROUND_2
         
             # Draw the entry zones
             entry_zone_hard = Rect(100 + x_offset, starting_offset + (x * y_offset), entry_x_offset_hard, 45)
@@ -242,7 +257,7 @@ def format_leaderboard(root, screen):
                 name_data_hard = ""
 
             # Render name data
-            name_object_hard = entry_font.render(name_data_hard, True, TEXT_COLOR, green)
+            name_object_hard = entry_font.render(name_data_hard, True, TEXT_COLOR, zone_color_hard)
             name_rect_hard = name_object_hard.get_rect()
             name_rect_hard.center = ((screen_width/4) + x_offset, 310 + (x * y_offset))
             screen.blit(name_object_hard, name_rect_hard)
@@ -257,7 +272,7 @@ def format_leaderboard(root, screen):
                 score_data_hard = ""
 
             # Render score data
-            score_object_hard = entry_font.render(score_data_hard, True, TEXT_COLOR, green)
+            score_object_hard = entry_font.render(score_data_hard, True, TEXT_COLOR, zone_color_hard)
             score_rect_hard = score_object_hard.get_rect()
             score_rect_hard.center = ((screen_width/4) + 300 + x_offset, 310 + (x * y_offset))
             screen.blit(score_object_hard, score_rect_hard)
